a3a390fec9d61611a7b9c88ce2d1248d001ccf8f
Alvaro Fernandez <alvaro@alvaro.local>
2018-11-23 09:36:24 +0100

    Initial version

--- PruebaGit/ViewController.swift
+++ PruebaGit/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  PruebaGit
-//
-//  Created by Alvaro on 23/11/18.
-//  Copyright © 2018 Alvaro. All rights reserved.
-//
 
 import UIKit
 
@@ -12,9 +5,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+    }    
 }
 
